9119d2147cdcda80ecc6d85ab956acec4e127723
Fellipe CTC <fellipectcamargo@gmail.com>
2025-10-19 14:08:40 -0300

    Greeting function using props for required task

--- labweek09/src/App.js
+++ labweek09/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function Greeting({ id, name }) {
+  return (
+    <div>
+      <h2>Welcome to Fullstack Development - I</h2>
+      <p>React JS Programming Week09 Lab Exercise</p>
+      <p>{id}</p>
+      <p>{name}</p>
+      <p>George Brown College, Toronto</p>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Greeting id="101497831" name="Fellipe C.T De Camargo" />
       </header>
     </div>
   );
